fcabeab1d4d836dfb64c5377960ba800d8f6c51a
scoomp <moshnyakov.alexei@yandex.ru>
2023-09-29 19:01:08 +0300

    add API

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,7 +1,12 @@
 import React, {useEffect, useState} from 'react'
+import axios from "axios";
 
 export default {
-    title: 'API'
+    title: '1e725b1e-ae3a-496b-83cf-d72bc209a4c6'
+}
+
+const options = {
+    withCredentials: true
 }
 
 export const GetTodolists = () => {
@@ -16,6 +21,9 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+         const promise = axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists' , {}, options)
+        promise.then( (res)=> {
+            setState(res.data)},  )
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
@@ -23,6 +31,10 @@ export const CreateTodolist = () => {
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        axios.delete('https://social-network.samuraijs.com/api/1.1/todo-lists' , {}, options)
+        then( (res)=>{
+            setState(res.data)
+        })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
