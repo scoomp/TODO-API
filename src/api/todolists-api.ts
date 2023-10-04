b7af69f37bc72efc3113640bd8a55962658ce2df
scoomp <moshnyakov.alexei@yandex.ru>
2023-10-04 20:40:09 +0300

    thunk, part 1

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': '27b2bd8b-1b73-421f-9f4b-ec4ead28b1d0'
+        'API-KEY': '96a8159b-85dd-4261-b515-58d38c2d4f66'
     }
 })
 
@@ -14,7 +14,7 @@ export const todolistsAPI = {
         return instance.get<TodolistType[]>('todo-lists');
     },
     createTodolist(title: string) {
-        return instance.post<ResponseType<{ item: TodolistType }>, AxiosResponse<ResponseType<{ item: TodolistType }>>, { title: string }>('todo-lists', {title});
+        return instance.post<ResponseType<{ item: TodolistType }>, AxiosResponse<ResponseType<{ item: TodolistType }>>,{ title: string }>('todo-lists', {title});
     },
     deleteTodolist(id: string) {
         return instance.delete<ResponseType>(`todo-lists/${id}`);
